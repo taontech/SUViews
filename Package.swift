1fd4502193f7ae16a1153d416405ca3cd945c9b7
taoning <taontech@gmail.com>
2023-09-18 22:23:49 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SUViews",
-    platforms: [.iOS(.v14), .tvOS(.v14), .macOS(.v11)],
+    platforms: [.iOS(.v16), .tvOS(.v16), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
